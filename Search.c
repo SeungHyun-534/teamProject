1a88c403c4df64cd2ff39180f7a8951ee6f132e5
SeungHyun-534 <22000534@handong.edu>
2021-05-11 22:52:16 +0900

    make Search.c

---
+++ Search.c
@@ -0,0 +1,28 @@
+#include "Search.h"
+#include <stdio.h>
+
+void search(Stu* arr[],int count){ //아직 미완성
+    int no;
+    printf("\n       ***검색기***\n");
+    printf("----------------------\n");
+    printf("1 : 이름으로 검색\n");
+    printf("2 : 학번으로 검색\n");
+    printf("3 : 최종 성적(범위)으로 검색\n");
+    printf("4 : Grade로 검색\n");
+    printf("0 : 취소\n");
+
+    printf("원하는 검색 넘버를 입력하세요 : ");
+    scanf("%d",&no);
+
+    switch (no)
+    {
+    case 1:
+        /* code */
+        break;
+    
+    default:
+        break;
+    }
+
+
+}
